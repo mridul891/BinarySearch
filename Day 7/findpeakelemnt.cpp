940a769f976cd0e77b485e6361f5c6c20b527a36
mridul891 <58576921+mridul891@users.noreply.github.com>
2024-03-14 22:22:18 +0530

    Day 8

--- Day 7/findpeakelemnt.cpp
+++ Day 7/findpeakelemnt.cpp
@@ -2,9 +2,19 @@
 using namespace std;
 int findPeakElement(int arr[], int n)
 {
+
     // Write your code here
-    int start = 0;
-    int end = n - 1;
+
+    // for only sigle digit
+    if (n == 1)
+        return 0;
+    if (arr[0] > arr[1])
+        return 1;
+    if (arr[n - 1] > arr[n - 2])
+        return n - 1;
+    int start = 1;
+    int end = n - 2;
+
     while (start <= end)
     {
         int mid = start + (end - start) / 2;
@@ -13,7 +23,7 @@ int findPeakElement(int arr[], int n)
         {
             return mid;
         }
-        else if (arr[mid] < arr[start])
+        else if (arr[mid] > arr[mid + 1])
         {
             end = mid - 1;
         }
@@ -26,7 +36,7 @@ int findPeakElement(int arr[], int n)
 
 int main()
 {
-    int arr[] = {1, 3, 5, 6, 7, 4, 3, 2};
+    int arr[] = {1, 3, 5, 6, 7, 9, 4, 3, 2};
     int n = sizeof(arr) / sizeof(arr[0]);
     int peak = findPeakElement(arr, n);
     cout << "the answer is " << peak;
